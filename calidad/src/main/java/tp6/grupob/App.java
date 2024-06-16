6faa54143a4463794db51ca0c5984fb4de9e089e
Iñaki <agustinerreguerena060504@gmail.com>
2024-06-16 19:53:15 -0300

    Configuracion inicial del pom

---
+++ calidad/src/main/java/tp6/grupob/App.java
@@ -0,0 +1,13 @@
+package tp6.grupob;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

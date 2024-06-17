8fbe7b58eade94bc13768028cc5a45f5ff01a3b7
FJP760 <fpiloni76@gmail.com>
2024-06-16 22:43:40 -0300

    prueba unitaria restar, integracion restar, gherkin restar

--- calidad/src/test/java/tp6/grupob/AppTest.java
+++ calidad/src/test/java/tp6/grupob/AppTest.java
@@ -14,16 +14,33 @@ public class AppTest
      * Rigorous Test :-)
      */
     @Test
+    public void testRestar()
+    {
+        int num1 = 8;
+        int num2 = 3;
+
+        int esperado = 5;
+        int resultado = Calculadora.restar(num1, num2);
+        assertEquals(resultado, esperado);
+    }
+
+    @Test
     public void unitariaSumar()
     {
         assertEquals(10,Calculadora.sumar(5,5));
     }
+
     @Test
     public void integracionSumarRestar(){
         int resulRest = Calculadora.restar(5, 1);
         assertEquals(8,Calculadora.sumar(resulRest,4));
     }
-    
+    @Test
+    public void integracionRestarSumar()
+    {
+        int resulSum = Calculadora.sumar(5, 1);
+        assertEquals(4, Calculadora.restar(resulSum, 2));
+    }
     
 
 }
--- calidad/target/test-classes/features/Calculadora.feature
+++ calidad/target/test-classes/features/Calculadora.feature
@@ -4,3 +4,8 @@ Feature: Pruebas de la Calculadora
     Given que tengo dos números 3 y 5
     When los sumo
     Then el resultado debe ser 8
+
+  Scenario: Restar Numeros
+    Given que tengo dos números 3 y 5
+    When los resto
+    Then el resultado debe ser -2

3083ef655382d931d25f1292176b76698a8f3149
Facundo-Fraga <fragafacundo78@gmail.com>
2024-06-16 21:56:29 -0300

    Prueba unitaria - Multiplicacion

--- calidad/src/test/java/tp6/grupob/AppTest.java
+++ calidad/src/test/java/tp6/grupob/AppTest.java
@@ -1,8 +1,11 @@
 package tp6.grupob;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
+
+import tp6.grupob.modelo.Calculadora;
 
 /**
  * Unit test for simple App.
@@ -13,8 +16,13 @@ public class AppTest
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue()
+    public void testMultiplicacion()
     {
-        assertTrue( true );
+        int num01 = 2;
+        int num02 = 6;
+
+        int esperado = 12;
+        int resultado = Calculadora.multiplicar(num01, num02);
+        assertEquals(esperado, resultado);
     }
 }
